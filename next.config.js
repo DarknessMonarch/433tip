c1f9ef2d6f13e3d8bbd103ceecc6bb436954c867
zero-stealth <penguincipher@gmail.com>
2025-05-14 06:52:07 +0300

    updated|

---
+++ next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/djuta2tca/image/upload/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dttvkmjpd/image/upload/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.api-sports.io',
+        port: '',
+        pathname: '/football/**',
+      },
+    ],
+  }
+};
